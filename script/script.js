5cfdf1e0fab8acca2712de5905ae96d0302dfa5a
zarias2020 <zarias@yandex.ru>
2021-04-12 17:13:07 +0300

    получение эл со стр

--- script/script.js
+++ script/script.js
@@ -1,5 +1,33 @@
 'use strict';
 
+const btnStart = document.getElementById('start'),
+
+  income = document.querySelector('.income'),
+  btnPlusIncome = income.getElementsByTagName('button'),
+  expenses = document.querySelector('.expenses'),
+  btnPlusExpenses = expenses.getElementsByTagName('button'),
+
+  depositCheck = document.querySelector('#deposit-check'),
+
+  additionalIncomeItem_1 = document.querySelectorAll('.additional_income-item')[0],
+  additionalIncomeItem_2 = document.querySelectorAll('.additional_income-item')[1],
+
+  budgetВayМalue = document.getElementsByClassName('budget_day-value'),
+  expensesMonthValue = document.getElementsByClassName('expenses_month-value'),
+  additionalIncomeValue = document.getElementsByClassName('additional_income-value'),
+  additionalExpensesValue = document.getElementsByClassName('additional_expenses-value'),
+  incomePeriodValue = document.getElementsByClassName('income_period-value'),
+  targetMonthValue = document.getElementsByClassName('target_month-value'),
+
+  salaryAmount = document.querySelector('.salary-amount'),
+  incomeTitle = document.querySelector('.income-title'),
+  incomeAmount = document.querySelector('.income-amount'),
+  expensesTitle = document.querySelector('.expenses-title'),
+  expensesAmount = document.querySelector('.expenses-amount'),
+  additionalExpensesItem = document.querySelector('.additional_expenses-item'),
+  targetAmount = document.querySelector('.target-amount'),
+  periodSelect = document.querySelector('.period-select');
+
 //проверка на буквы
 let checkStr = function (str) {
   let reg = /^[\d]+$/;
