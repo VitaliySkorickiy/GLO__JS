059b4d75b304dcca61566ee9cfa873e2065fa34b
zarias2020 <zarias@yandex.ru>
2021-04-29 18:02:02 +0300

    калькулятор с бегающим числом

--- script/script.js
+++ script/script.js
@@ -273,7 +273,6 @@ window.addEventListener('DOMContentLoaded', () => {
       message = document.getElementById('form2-message'),
       email = document.getElementById('form2-email'),
       tel = document.getElementById('form2-phone');
-    console.log(name);
 
     const str = (item) => {
       item.value = item.value.replace(/[^А-Яа-яЁё\-\ ]/gi, '').trim();
@@ -296,4 +295,62 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   };
   validForm();
+
+  // калькулятор
+  const calc = (price = 100) => {
+    const calcBlock = document.querySelector('.calc-block'),
+      calcType = document.querySelector('.calc-type'),
+      calcCount = document.querySelector('.calc-count'),
+      calcDay = document.querySelector('.calc-day'),
+      calcSquare = document.querySelector('.calc-square'),
+      totalValue = document.getElementById('total');
+
+    const countSum = () => {
+      let total = 0,
+        countValue = 1,
+        dayValue = 1;
+
+      let typeValue = calcType.options[calcType.selectedIndex].value,
+        squareValue = +calcSquare.value;
+
+      if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+      }
+
+      if (calcDay.value && calcDay.value <= 5) {
+        dayValue *= 2;
+      } else if (calcDay.value && calcDay.value <= 10) {
+        dayValue *= 1.5;
+      }
+
+      if (squareValue && typeValue) {
+        total = price * typeValue * squareValue * countValue * dayValue;
+      }
+      // totalValue.textContent = total;
+
+      const f1 = () => {
+        let t = 0;
+        let interval = setInterval(() => {
+          t += 173;
+
+          if (t >= total) {
+            t = total;
+            clearInterval(interval);
+          }
+          totalValue.textContent = t;
+        }, 24);
+      };
+
+      f1();
+    };
+
+    calcBlock.addEventListener('change', (e) => {
+      const target = e.target;
+
+      if (target.matches('select') || target.matches('input')) {
+        countSum();
+      }
+    });
+  };
+  calc(100);
 });
